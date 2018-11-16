59e6d0d74746b4278feb634d42824e3ec4885c10
lass5643 <lass5643@stud.kea.dk>
2018-11-16 23:04:27 +0100

    Adding Data to the Spring Model + Binding Request Param

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -16,6 +20,49 @@ public class HelloWorldController {
 	// need a controller method to process the HTML form
 	@RequestMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+	
+	// add a new controller method to read form data and
+	// add data to the model
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		
+		// convert the data to all UPPERCASE
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Yo! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, 
+			Model model) {
+		
+		// REDUNDANT DUE TO @RequestParam("studenName" String theName)
+		// read the request parameter from the HTML form
+		// String theName = request.getParameter("studentName");
+		
+		// convert the data to all UPPERCASE
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey My Friend from v3! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
 		return "helloworld";
 	}
 }
